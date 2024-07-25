93a7cd30b2efbf1b1aad43bd04e4aee5461051d6
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-25 14:32:50 +0200

    db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Authors (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 )
 
 CREATE TABLE Order_Details (
