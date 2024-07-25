0b6e2e5208e2cf25e84925062299cc1e8644ee80
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-25 14:34:47 +0200

    db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,7 +32,7 @@ CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity FLOAT
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id)
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    quantity FLOAT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
