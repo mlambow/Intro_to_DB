a646ea5c6f23d62d5d2422313ac9b9d0097a41cd
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-27 06:11:01 +0200

    DB

---
+++ MySQLServer.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '123456',
+    database = 'mydb'
+)
+
+print(mydb.get_server_info())
